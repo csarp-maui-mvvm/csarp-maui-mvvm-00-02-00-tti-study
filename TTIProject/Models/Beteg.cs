e6677da37bebc55d58fa86523f3e5dc54e85b933
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-09-29 09:40:08 +0200

    Model mappa Beteg osztállyal

---
+++ TTIProject/Models/Beteg.cs
@@ -0,0 +1,51 @@
+﻿namespace TTIProject.Models
+{
+    public class Beteg 
+    {
+        private string _nev;
+        public string Nev
+        {
+            get => _nev;
+            set { _nev = value; }
+        }
+
+        private double _suly;
+        public double Suly
+        {
+            get => _suly;
+            set { _suly = value; }
+        }
+
+        private double _magassag;
+        public double Magassag
+        {
+            get => _magassag;
+            set { _magassag = value; }
+        }
+
+        public Beteg()
+        {
+            _nev = string.Empty;
+            _suly = 60;
+            _magassag = 1.6;
+        }
+
+        public double TTI
+        {
+            get
+            {
+                double tti = _suly / Math.Pow(_magassag, 2);
+                double roundedTTI = Math.Round(tti, 2);
+                return roundedTTI;
+            }
+        }
+
+        public string BetegAdatok
+        {
+            get
+            {
+                return $"{_nev} beteg testőmeg indexe:  {TTI}";
+            }
+        }   
+    }
+}
