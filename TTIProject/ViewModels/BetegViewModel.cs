ebe0df8345ef9315d98f7fe3117745c0c0bc8957
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-09-29 14:16:28 +0200

    Úgy adat esetén vagy induláskor nincs eredmény!

--- TTIProject/ViewModels/BetegViewModel.cs
+++ TTIProject/ViewModels/BetegViewModel.cs
@@ -7,21 +7,56 @@ namespace TTIProject.ViewModels
     public partial class BetegViewModel : ObservableObject
     {
         [ObservableProperty]
+        private bool _resultIsVisible;
+
+        [ObservableProperty]
         private Beteg _beteg;
 
         public BetegViewModel()
         {
             Beteg = new Beteg();
+            ResultIsVisible = false;
         }
 
         public double TTI => Beteg.TTI;
         public string BetegAdatok => Beteg.BetegAdatok;
+
+        public string Nev
+        {
+            get => Beteg.Nev;
+            set
+            {
+                Beteg.Nev = value;
+                ResultIsVisible = false;
+            }
+        }
+
+        public double Magassag
+        {
+            get => Beteg.Magassag;
+            set
+            {
+                Beteg.Magassag = value;
+                ResultIsVisible = false;
+            }
+        }
+
+        public double Suly
+        {
+            get => Beteg.Suly;
+            set
+            {
+                Beteg.Suly = value;
+                ResultIsVisible = false;
+            }
+        }
 
         [RelayCommand]
         public void DoTTICompute()
         {
             OnPropertyChanged(nameof(TTI));
             OnPropertyChanged(nameof(BetegAdatok));
+            ResultIsVisible = true;
         }
     }
 }
