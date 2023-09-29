1597a2d89946c2d7a885ebe9a0640e3816468fa7
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-09-29 10:03:48 +0200

    A ViewModel egy Beteg osztályt kezel

--- TTIProject/ViewModels/BetegViewModel.cs
+++ TTIProject/ViewModels/BetegViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using TTIProject.Models;
 
 namespace TTIProject.ViewModels
 {
     public partial class BetegViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private Beteg _beteg;
     }
 }
